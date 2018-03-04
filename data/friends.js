02b9cc3bb2aa79b73360f2327fd3ef392dbea53b
mjsimon91 <mjsimon91@gmail.com>
2018-03-04 12:32:06 -0500

    Have the abiity to find the different values for a new friend and all of the existing friends

--- data/friends.js
+++ data/friends.js
@@ -1,4 +1,18 @@
 // Creating an array for each person that has been created
-var friends =[];
+var friends = [{
+    firstName: 'Mike',
+    lastName: 'Smith',
+    imageURL: 'www.google.com',
+    questionOne: 1,
+    questionTwo: 3,
+    questionThree: 5,
+    questionFour: 3,
+    questionFive: 4,
+    questionSix: 6,
+    questionSeven: 1,
+    questionEight: 3,
+    questionNine: 2,
+    questionTen: 5
+  }];
 
 module.exports = friends;
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -15,7 +15,58 @@ module.exports = function(app){
     var matchImage = '';
 
 
-    console.log(friendInput);
+    function potentialFriend (firstName, lastName, profileImage){
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.profileImage = profileImage;
+      scoreDiff = [];
+      totalScoreDiff = 0;
+    }
+
+    potentialFriend.prototype.diffArray = function(){
+      //Loop through each friend to find the different scores for each response
+      for (let i = 0; i < friendData.length; i++) {
+        //go through each question and get the score difference
+        var questionOneDiff = parseInt(Math.abs(req.body.questionOne - friendData[i].questionOne));
+        var questionTwoDiff = parseInt(Math.abs(req.body.questionTwo - friendData[i].questionTwo));
+        var questionThreeDiff = parseInt(Math.abs(req.body.questionThree - friendData[i].questionThree));
+        var questionFourDiff = parseInt(Math.abs(req.body.questionFour - friendData[i].questionFour));
+        var questionFiveDiff = parseInt(Math.abs(req.body.questionFive - friendData[i].questionFive));
+        var questionSixDiff = parseInt(Math.abs(req.body.questionSix - friendData[i].questionSix));
+        var questionSevenDiff = parseInt(Math.abs(req.body.questionSeven - friendData[i].questionSeven));
+        var questionEightDiff = parseInt(Math.abs(req.body.questionEight - friendData[i].questionEight));
+        var questionNineDiff = parseInt(Math.abs(req.body.questionNine - friendData[i].questionNine));
+        var questionTenDiff = parseInt(Math.abs(req.body.questionTen - friendData[i].questionTen));
+        
+        //Push all the differential values to the scoreDiff array
+        scoreDiff.push(questionOneDiff);
+        scoreDiff.push(questionTwoDiff);
+        scoreDiff.push(questionThreeDiff);
+        scoreDiff.push(questionThreeDiff);
+        scoreDiff.push(questionFourDiff);
+        scoreDiff.push(questionFiveDiff);
+        scoreDiff.push(questionSixDiff);
+        scoreDiff.push(questionSevenDiff);
+        scoreDiff.push(questionEightDiff);
+        scoreDiff.push(questionNineDiff);
+        scoreDiff.push(questionTenDiff);
+
+        //add the different score to an array using reduce
+        totalScoreDiff = scoreDiff.reduce(function(curVal,amount){
+          return curVal + amount
+        })
+        //find the person with the lowest differential value
+      }
+    }
+    
+    for (let j = 0; j < friendData.length; j++) {
+      var friends = new potentialFriend(friendData[j].firstName, friendData[j].lastName, friendData[j].imageURL)
+      friends.diffArray(); 
+      console.log('Score Array ' +scoreDiff);
+      console.log('Total Score ' + totalScoreDiff);
+      
+    }
+
     friendData.push(friendInput)
 
     //Loop through each object to find the minimum value in the arrays
